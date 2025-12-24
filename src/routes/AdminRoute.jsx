fcf6a29043adb1fa34634adbb254c3585d948ce8
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2025-12-24 16:06:01 +0600

    admin route loading error fixed

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import useRole from '../hooks/useRole';
 import Forbidden from '../pages/error/Forbidden';
+import Loading from '../components/common/Loading';
 
 const AdminRoute = ({ children }) => {
     const { role, roleLoading } = useRole();
